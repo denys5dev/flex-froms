1a00d4c1b4754ca19076fb60610d8ecaa6f037df
denys5dev <denys5dev@gmail.com>
2019-01-15 19:25:54 +0100

    expand collapse

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { Service } from './my-service.service';
+import { Component, ViewChild } from '@angular/core';
+import { Service, Employee } from './my-service.service';
+import { DxTreeListComponent } from 'devextreme-angular/ui/tree-list';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +8,36 @@ import { Service } from './my-service.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @ViewChild('tree') treeList: DxTreeListComponent;
+  expanded = true;
+
   title = 'devex-request-forms';
   now = new Date();
+  employees: Employee[] = [];
   simpleProducts: string[];
+
   constructor(service: Service) {
     this.simpleProducts = service.getSimpleProducts();
+    this.employees = service.getEmployees();
+  }
+
+
+  toggleRow(key) {
+    var keys = this.getNodeKeys(this.treeList.instance.getRootNode());
+    this.treeList.instance.beginUpdate();
+    keys.forEach((key) => {
+      this.expanded ? this.treeList.instance.collapseRow(key) : this.treeList.instance.expandRow(key);
+    });
+    this.treeList.instance.endUpdate();
+    this.expanded = !this.expanded;
+  }
+
+  getNodeKeys(node) {
+    var keys = [];
+    keys.push(node.key);
+    node.children.forEach((item) => {
+      keys = keys.concat(this.getNodeKeys(item));
+    });
+    return keys;
   }
 }
